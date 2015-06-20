c18af6efe1d7e67da00a1f905ef6bb8a752c0813
Sean Stephenson <sms@pixar.com>
2015-06-20 16:04:02 -0700

    add item partials

--- static/js/app.js
+++ static/js/app.js
@@ -1,12 +1,44 @@
 'use strict';
 
-var dianawon = angular.module('dianawon', ['dianawon.controllers', 'dianawon.services']);
+var dianawon = angular.module('dianawon', ['ngRoute', 'dianawon.controllers', 'dianawon.services']);
+
+dianawon.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/', {
+        templateUrl: 'partials/home.html',
+        controller: 'MainCtrl'
+      }).
+      when('/contact', {
+        templateUrl: 'partials/contact.html',
+        controller: 'MainCtrl'
+      }).
+      when('/portfolio/:item', {
+        templateUrl: 'partials/item.html',
+        controller: 'ItemCtrl'
+      });
+}]);
+
 angular.module('dianawon.controllers', []).
   controller('MainCtrl', ['$scope', 'dianawonService', function ($scope, dianawonService) {
-
-    // AJAX call
     var request = {'method': 'get', 'resource': 'items.json'};
     dianawonService.async(request).then(function(response) {
       $scope.items = response.data;
+    });
+    $scope.email = 'dianaswon' + '@' + 'gmail.com';
+  }]).
+
+  controller('ItemCtrl', ['$scope', 'dianawonService', '$routeParams', 
+      function ($scope, dianawonService, $routeParams) {
+
+    var request = {'method': 'get', 'resource': 'items.json'};
+    dianawonService.async(request).then(function(response) {
+      var items = response.data;
+      for (var i = 0, j = items.length; i < j; i++) {
+        if (items[i].url === $routeParams.item) {
+          $scope.item = items[i];
+          return;
+        }
+      }
     });
   }]);
